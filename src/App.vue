b8ad4a35e522e873314ea7cabe1bfbf564e2d688
hugo <hugo.alonso.7e5@itb.cat>
2024-10-24 19:48:14 +0200

    proyecto de tal

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { reactive } from 'vue';
+import MissatgeComponent from './components/MissatgeComponent.vue'
+
+let messageArray = reactive([])
+
+function recibir(missatge) {
+  console.log(missatge)
+
+  
+  messageArray.push(missatge)
+
+  console.log(messageArray)
+}
+</script>
+
+<template>
+  <div>
+    <MissatgeComponent @enviar="recibir" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/MissatgeComponent.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+    import { ref } from 'vue';
+
+    const emit = defineEmits(['enviar'])
+
+    let missatge = ref("")
+    function enviar(){
+        
+        emit("enviar",missatge.value)
+    }
+</script>
+
+<template>
+    <div>
+        <input placeholder="Escribe tu mensaje tet" v-model="missatge">
+        <button @click="enviar">Enviar mensaje</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
